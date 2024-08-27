817083cf7fb42193381f953deec31525a2710a19
gopalc <gopal.chandakk@gmail.com>
2024-08-28 05:01:18 +0530

    Updates

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
-  build: {
-    rollupOptions: {
-      external: ['vite-plugin-node-polyfills/shims/global'], 
-    },
-  },
+  plugins: [
+    nodePolyfills({
+      // Specific modules that should not be polyfilled.
+      exclude: [],
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
 });
