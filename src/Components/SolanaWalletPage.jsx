04ded7a2622b895274e8f8c68ffab42ddf95d49e
gopalc <gopal.chandakk@gmail.com>
2024-08-28 05:15:59 +0530

    Updates

--- src/Components/SolanaWalletPage.jsx
+++ src/Components/SolanaWalletPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React,{ useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Keypair } from '@solana/web3.js';
 import { mnemonicToSeed } from 'bip39';
